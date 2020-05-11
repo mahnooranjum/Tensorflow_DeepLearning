89975f57c0d948615d198f73e305f489523ca44c
Mahnoor Anjum <manomaq@gmail.com>
2020-05-12 04:26:44 +0500

    updated tf2 py files

---
+++ Tensorflow_2X_PythonFiles/demo7_blobsclassification.py
@@ -0,0 +1,97 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+print(tf.__version__)
+
+from sklearn.datasets.samples_generator import make_moons
+from sklearn.datasets.samples_generator import make_circles
+from sklearn.datasets.samples_generator import make_blobs
+n = 10000
+# generate 2d classification dataset
+X, y = make_blobs(n_samples=n, centers=2,cluster_std=2.5, n_features=2)
+# scatter plot, dots colored by class value
+df = pd.DataFrame(dict(x=X[:,0], y=X[:,1], label=y))
+colors = {0:'red', 1:'blue'}
+fig, ax = plt.subplots()
+grouped = df.groupby('label')
+for key, group in grouped:
+    group.plot(ax=ax, kind='scatter', x='x', y='y', label=key, color=colors[key])
+plt.show()
+
+datadict = {'X1': X[:,0],'X2' : X[:,1], 'target': y}
+data = pd.DataFrame(data=datadict)
+
+X = data.iloc[:,[0, 1]].values
+type(X)
+
+y = data.target.values
+
+# TRAIN TEST SPLIT
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+N, D = X_train.shape
+
+from sklearn.preprocessing import StandardScaler 
+scaleObj = StandardScaler()
+X_train = scaleObj.fit_transform(X_train)
+X_test = scaleObj.transform(X_test)
+
+from tensorflow.keras.layers import Input, Dense, Activation,Dropout
+from tensorflow.keras.models import Model
+
+input_layer = Input(shape=(D,))
+dense_layer_1 = Dense(4, activation='tanh')(input_layer)
+dense_layer_2 = Dense(2, activation='sigmoid')(input_layer)
+output = Dense(1, activation='sigmoid')(dense_layer_2)
+
+model = Model(inputs=input_layer, outputs=output)
+
+model.compile(
+    optimizer = 'adam',
+    loss = 'binary_crossentropy',
+    metrics = ['accuracy']
+)
+
+# https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/LearningRateScheduler
+def scheduler(epoch):
+  if epoch < 10:
+    return 0.001
+  else:
+    return 0.001 * tf.math.exp(0.1 * (10 - epoch))
+
+
+callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
+
+report = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=50)
+
+plt.plot(report.history['loss'], label="loss = SGD")
+
+print(X.shape)
+print(y.shape)
+
+y_predicted = model.predict(X)
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, model.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('pink', 'cyan')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'blue'))(i), label = j)
+plt.title('Model')
+plt.xlabel('X')
+plt.ylabel('y')
+plt.legend()
+plt.show()
+
+print("Train eval: ", model.evaluate(X_train, y_train))
+print("Test eval: ", model.evaluate(X_test, y_test))
